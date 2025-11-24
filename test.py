8b3a7e1f4268c0dc922e8d1fb0f99675cfeeab95
Ganesh Iyer <omkaar0791@gmail.com>
2025-11-24 13:52:35 +0100

    Removed a test.py file

--- test.py
+++
@@ -1,6 +0,0 @@
-from isaacgymenvs.tasks import task_registry
-
-task_name = "Isaac-Velocity-Rough-K1-v0"  # replace with your task's registered name
-env = task_registry[task_name]()
-print("Task initialized successfully!")
-env.close()
